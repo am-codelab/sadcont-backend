711a310fd5b3dac1e1f6681624be0c2f0c65a759
am-codelab <m7lucas2013@gmail.com>
2026-04-22 20:51:41 -0500

    Corregir error de deploy

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render te da el PORT
+    app.run(host="0.0.0.0", port=port, debug=True)
+
+    # app.run(host="0.0.0.0", port=5000, debug=True)
